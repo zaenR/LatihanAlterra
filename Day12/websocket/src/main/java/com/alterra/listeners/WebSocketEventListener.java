bb892767c2e95011da61ce3ecbc8c5db09393f58
zaenR <108667319+zaenR@users.noreply.github.com>
2022-08-31 18:56:01 +0700

    Tugas WebSocket

---
+++ Day12/websocket/src/main/java/com/alterra/listeners/WebSocketEventListener.java
@@ -0,0 +1,41 @@
+package com.alterra.listeners;
+
+import com.alterra.dtos.ChatMessageDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+public class WebSocketEventListener {
+    private static final Logger LOG = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        LOG.info("Koneksi web socket baru "+event.toString());
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) accessor.getSessionAttributes().get("username");
+
+        if (username != null) {
+            LOG.info("User disconnected : "+username);
+
+            ChatMessageDTO chatMessage = new ChatMessageDTO();
+            chatMessage.setSender(username);
+            chatMessage.setType(ChatMessageDTO.MessageType.LEAVE);
+
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
